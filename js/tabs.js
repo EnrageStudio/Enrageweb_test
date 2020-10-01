bfb6278a0fdccaff5cecf3010b3b265decb5bb5c
LichKiller <LichKiller@localhost.localdomain>
2020-10-01 15:09:48 -0500

    Añadido estilos en tabs y JS - v 0.2

--- js/tabs.js
+++ js/tabs.js
@@ -1,8 +1,20 @@
 (function(){
     var propTabs = {
-        
+        first_e: document.getElementById('encabezado-menu').firstElementChild,
+        first_c: document.getElementById('contenido-menu').firstElementChild,
+        enlace_e: document.querySelectorAll('#encabezado-menu li a'),
     }
     var metTabs = {
-
+        inicio: function(){
+            propTabs.first_e.className = 'active';
+            propTabs.first_c.className = 'active';
+            for(var i = 0; i < propTabs.enlace_e.length;i++){
+                propTabs.enlace_e[i].addEventListener('click',metTabs.evento);
+            }
+        },
+        evento: function(){
+            
+        }
     }
+    metTabs.inicio();
 }())
